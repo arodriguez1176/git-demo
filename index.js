8bbda2caad73f1f581a7126ff290ce39cb5e3cbe
arodriguez1176 <87504625+arodriguez1176@users.noreply.github.com>
2022-03-10 11:13:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Hello World!");
 console.log("Armando's branch log");
 console.log("Hi ")
 console.log("This is coming from Sunni-Dev");
-console.log("This is a check on pull requests with multiple collaborators");
+console.log("This is a check on pull requests with multiple collaborators.");
 
 
 
